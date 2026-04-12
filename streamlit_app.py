c7d1c7a299a9739f2650296e4e348094a0486750
muhdefri <86890168+muhdefri@users.noreply.github.com>
2026-04-13 02:57:07 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -473,6 +473,8 @@ if uploaded:
             
             mask_23 = df_payload["Band"] == "L2300"
             
+            df_payload["LAYER"] = df_payload["LAYER"].fillna("UNK")
+
             df_payload.loc[mask_23, "Band_Layer"] = (
                 df_payload.loc[mask_23, "Band"] + "_" + df_payload.loc[mask_23, "LAYER"]
             )
