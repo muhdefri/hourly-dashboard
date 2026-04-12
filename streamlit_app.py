4ac0994ab24b0c02c7a6c26f12f45f00e9ad2219
muhdefri <86890168+muhdefri@users.noreply.github.com>
2026-04-13 01:57:23 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -484,37 +484,36 @@ if uploaded:
             sectors = ["SEC1","SEC2","SEC3"]
 
             # ================= ROW 1 =================
-                       cols = st.columns(3)
-                       
-                       for i, sec in enumerate(sectors):
-                           with cols[i]:
-                       
-                               st.markdown(f"### Band - Sector {i+1}")
-                       
-                               df_sec = df_payload[df_payload["SECTOR_GROUP"] == sec]
-                       
-                               if df_sec.empty:
-                                   st.warning("No Data")
-                                   continue
-                       
-                               df_plot = (
-                                   df_sec.groupby(["DATE_ID","Band_Layer"])["Total_Traffic_Volume_new"]
-                                   .sum()
-                                   .reset_index()
-                               )
-                       
-                               # ✅ HARUS sejajar dengan df_plot (bukan masuk ke dalam)
-                               order = sorted(df_plot["Band_Layer"].dropna().unique())
-                       
-                               fig = px.area(
-                                   df_plot,
-                                   x="DATE_ID",
-                                   y="Total_Traffic_Volume_new",
-                                   color="Band_Layer",
-                                   category_orders={"Band_Layer": order}
-                               )
-                       
-                               st.plotly_chart(apply_universal_legend(fig), use_container_width=True)
+            cols = st.columns(3)
+            
+            for i, sec in enumerate(sectors):
+                with cols[i]:
+            
+                    st.markdown(f"### Band - Sector {i+1}")
+            
+                    df_sec = df_payload[df_payload["SECTOR_GROUP"] == sec]
+            
+                    if df_sec.empty:
+                        st.warning("No Data")
+                        continue
+            
+                    df_plot = (
+                        df_sec.groupby(["DATE_ID","Band_Layer"])["Total_Traffic_Volume_new"]
+                        .sum()
+                        .reset_index()
+                    )
+            
+                    order = sorted(df_plot["Band_Layer"].dropna().unique())
+            
+                    fig = px.area(
+                        df_plot,
+                        x="DATE_ID",
+                        y="Total_Traffic_Volume_new",
+                        color="Band_Layer",
+                        category_orders={"Band_Layer": order}
+                    )
+            
+        st.plotly_chart(apply_universal_legend(fig), use_container_width=True)
 
             # ================= ROW 2 =================
             col1, col2 = st.columns([2,1])
